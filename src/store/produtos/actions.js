523b5d855802cfb297936c614bf49598ae67ea73
Gutemberg Borges <gutembergborges@MacBook-Air-de-Gutemberg.local>
2019-09-23 15:02:28 -0300

    create endpoint ObterProdutosPG method

--- src/store/produtos/actions.js
+++ src/store/produtos/actions.js
@@ -23,4 +23,32 @@ export function listar(store) {
         Loading.hide();
       });
   });
+
+  export function filtrar(store, params) {
+    Loading.show();
+    return new Promise((resolve, reject) => {
+      axios.post(`http://localhost:8888/posseidom.php?op=ObterProdutosPG`, {
+        Token: localStorage.getItem('token'),
+        pagina: 0,
+        grupo: params.group,
+        descricao: params.description
+      })
+        .then(({ data }) => {
+          console.log(data)
+          // store.commit
+        })
+        .catch(() => {
+          Notify.create({
+            color: 'red',
+            position: 'bottom',
+            message: 'Erro de conexão',
+            icon: 'ion-ios-warning'
+          })
+          reject();
+        })
+        .finally(() => {
+          Loading.hide();
+        });
+    });
+  }
 }
